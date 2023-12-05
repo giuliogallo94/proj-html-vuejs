62cace389f11e650bbda04c394286bf28bb22fe7
giuliogallo94 <giuliogallo1994@gmail.com>
2023-12-05 14:31:05 +0100

    style: carouselCard :hover

--- src/components/AppCarouselCard.vue
+++ src/components/AppCarouselCard.vue
@@ -14,11 +14,12 @@ export default {
   <div class="ms_card mx-3 shadow rounded-4">
     <img :src="`src/assets/img/${portfolio.url}`" alt="" class="rounded-4" />
 
-    <div class="portfolio-title d-flex justify-content-between pt-3 px-3">
-      <p>
+    <div
+      class="portfolio-title d-flex justify-content-between align-items-center py-4 px-3">
+      <p class="m-0">
         <b>{{ portfolio.title }}</b>
       </p>
-      <p>{{ portfolio.subtitle }}</p>
+      <p class="m-0">{{ portfolio.subtitle }}</p>
     </div>
   </div>
 </template>
@@ -27,10 +28,26 @@ export default {
 @use "../style/partials/variables" as *;
 .ms_card {
   min-width: calc(100vw / 3.75);
-
+  font-size: 15px;
   img {
     max-width: 100%;
   }
-  font-size: 15px;
+
+  &:hover {
+    background: linear-gradient(
+      90deg,
+      rgba(200, 54, 109, 1) 0%,
+      rgba(212, 64, 109, 1) 50%,
+      $mainPink 100%
+    );
+
+    b {
+      color: white;
+    }
+
+    p {
+      color: rgba(255, 255, 255, 0.5);
+    }
+  }
 }
 </style>
